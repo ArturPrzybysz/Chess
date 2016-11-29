6698749e626608b9ff1596d1204589a9ada54e01
PiotrJander <pjander2@gmail.com>
2016-11-29 11:36:10 -0500

    Refactorowanko

---
+++ src/test/java/test/KingTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.Test;
+
+import board.Board;
+import general.Colour;
+import player.Player;
+
+public class KingTest {
+
+	@Test
+	public void test() {
+		Player p1 = new Player(Colour.WHITE);
+		Player p2 = new Player(Colour.BLACK);
+		Board b = new Board(p1, p2);
+		b.test_king(p1);
+		b.read_pieces();
+		System.out.println("koniec");
+	}
+
+}
---
+++ src/test/java/test/PawnTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import board.Board;
+import general.Colour;
+import general.Coord;
+import piece.Pawn;
+import player.Player;
+
+public class PawnTest {
+
+	@Test
+	public void test() {
+		Player p1 = new Player(Colour.WHITE);
+		Player p2 = new Player(Colour.BLACK);
+		Board b = new Board(p1, p2);
+		b.test_pawn(p1);
+		b.read_pieces();
+		for(int i=0;i<5;i++) {
+		b.main_move(p1);
+		b.read_pieces();
+		b.main_move(p2);
+		b.read_pieces();
+		}
+	}
+
+}
